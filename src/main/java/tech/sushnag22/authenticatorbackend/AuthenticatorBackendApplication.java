fa95de08db1f593aec866e602ef81815f1664568
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-04 18:01:47 +0530

    Initial commit

---
+++ src/main/java/tech/sushnag22/authenticatorbackend/AuthenticatorBackendApplication.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.authenticatorbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticatorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/sushnag22/authenticatorbackend/AuthenticatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.authenticatorbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
