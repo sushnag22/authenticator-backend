3a583b1bd70e30a7afada1eb48710170765c423d
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-08-04 18:27:30 +0530

    Add controller UserApiService.java class with POST methods

---
+++ src/main/java/tech/sushnag22/authenticatorbackend/controller/UserApiService.java
@@ -0,0 +1,50 @@
+package tech.sushnag22.authenticatorbackend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import tech.sushnag22.authenticatorbackend.model.User;
+import tech.sushnag22.authenticatorbackend.repository.UserRepository;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class UserApiService {
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/api/register")
+    public String register(@Valid @RequestBody User user) {
+
+        List<User> users = userRepository.findAll();
+        System.out.println("New User -> " + user);
+
+        for (User u: users) {
+            if (user.equals(u)) {
+                System.out.println("User already exists!");
+                return "USER_ALREADY_EXISTS";
+            }
+        }
+
+        userRepository.save(user);
+        return "SUCCESS";
+
+    }
+
+    @PostMapping("/api/login")
+    public String login(@Valid @RequestBody User user) {
+
+        List<User> users = userRepository.findAll();
+
+        for (User u: users) {
+            if (u.equals(user)) {
+                return user.getUserName();
+            }
+        }
+
+        return "FAILURE";
+
+    }
+}
